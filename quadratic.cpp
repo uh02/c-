b629cb53bedb70deb9d9a1e48b3245f991f43c2d
uh02 <34234444+uh02@users.noreply.github.com>
2022-10-31 14:08:52 +0100

    Add files via upload

---
+++ quadratic.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+int main()
+{
+  int a;
+  int b;
+  int c;
+  cout << "Inserire i coefficenti dell'equazione:";
+  cin >> a;
+  cout << "\n x^2+";
+  cin >> b;
+  cout << "\n x+";
+  cin >> c;
+  return 0;
+}
